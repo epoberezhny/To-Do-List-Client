bfef4a8ae40c8d093ee681b37baee3b7672430fd
Eugene <boberr10@yandex.ru>
2017-11-14 20:19:07 +0200

    remove unnecessary

--- src/components/comment/component.js
+++ src/components/comment/component.js
@@ -1,9 +1,7 @@
 import template from './template.html';
-import controller from './controller';
 
 const CommentComponent = {
   template,
-  controller,
   bindings:{
     comment: '<',
     deleteComment: '&'
